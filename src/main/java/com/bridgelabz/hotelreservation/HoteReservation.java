fbf3f0e52b44a4e5c3042bff49b754838cbbf7c8
Swathi <swathi.h@perfios.com>
2021-09-16 08:17:52 +0530

    Initial Commit with welcome message

---
+++ src/main/java/com/bridgelabz/hotelreservation/HoteReservation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.hotelreservation;
+
+public class HoteReservation {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcom to Hotel Reservation System");
+	}
+}
