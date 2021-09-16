638f459617c8fa43fc9ee778d552abdfb80aa9cc
Swathi <swathi.h@perfios.com>
2021-09-16 17:24:58 +0530

    ADDED: Logic to find cheapest hotel using streams

---
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.hotelreservation;
+
+import java.time.LocalDate;
+//import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class HotelReservation {
+	static List<Hotel> hotelList = new ArrayList<Hotel>();
+
+	public boolean addHotel(String hotelName, double rateForRegular) {
+		Hotel hotel = new Hotel(hotelName, rateForRegular);
+		boolean isAdded = hotelList.add(hotel);
+		return isAdded;
+
+	}
+
+	public Hotel findCheapHotel(LocalDate startDate, LocalDate endDate) {
+
+//		long daysStaying = ChronoUnit.DAYS.between(startDate, endDate);
+		Optional<Hotel> cheapestRate = hotelList.stream().min(Comparator.comparingDouble(Hotel::getRateForRegular));
+		return cheapestRate.get();
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcom to Hotel Reservation System");
+	}
+
+}
--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -1,14 +1,40 @@
 package com.bridgelabz.hotelreservation;
 
+import java.time.LocalDate;
+import java.time.Month;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class HotelReservationTest {
-	
+
 	@Test
-	public void WhenGivenName_AddHotel_AndReturnTrue() {
-		HoteReservation hoteReservation = new HoteReservation();
+	public void WhenGivenBridgewood_AddHotel_AndReturnTrue() {
+		HotelReservation hoteReservation = new HotelReservation();
 		boolean isAdded = hoteReservation.addHotel("Lakewood", 110.0);
 		Assert.assertTrue(isAdded);
 	}
+
+	@Test
+	public void WhenGivenName_AddHotel_AndReturnTrue() {
+		HotelReservation hoteReservation = new HotelReservation();
+		boolean isAdded = hoteReservation.addHotel("Bridgewood", 160.0);
+		Assert.assertTrue(isAdded);
+	}
+
+	@Test
+	public void WhenGivenRidgewood_AddHotel_AndReturnTrue() {
+		HotelReservation hoteReservation = new HotelReservation();
+		boolean isAdded = hoteReservation.addHotel("Ridgewood", 220.0);
+		Assert.assertTrue(isAdded);
+	}
+
+	@Test
+	public void WhenHotelList_ShouldGive_CheapestHotelRate() {
+		HotelReservation hoteReservation = new HotelReservation();
+		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 16);
+		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 17);
+		Hotel hotel = hoteReservation.findCheapHotel(startDate, endDate);
+		Assert.assertEquals(110.0, hotel.getRateForRegular());
+	}
+
 }
