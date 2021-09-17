79164c11836be16bf7763efd1c2a55aaa753d6e5
Swathi <swathi.h@perfios.com>
2021-09-17 16:31:32 +0530

    ADDED: Method to get highest rated hotel

--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -7,30 +7,15 @@ import java.util.List;
 public class HotelReservation {
 	static List<Hotel> hotelList = new ArrayList<Hotel>();
 
-<<<<<<< HEAD
-	public boolean addHotel(String hotelName, double rateForRegular) {
-		Hotel hotel = new Hotel(hotelName, rateForRegular);
-=======
 	public boolean addHotel(String hotelName, int rating, double rateForWeekday, double rateForWeekend) {
 		Hotel hotel = new Hotel(hotelName, rating, rateForWeekday, rateForWeekend);
->>>>>>> hotel_resevation_system_uc6
 		boolean isAdded = hotelList.add(hotel);
 		return isAdded;
 
 	}
 
-<<<<<<< HEAD
-	public Hotel findCheapHotel(LocalDate startDate, LocalDate endDate) {
-
-		Hotel hotel = hotelList.stream().min((h1,h2) -> {
-			return (int) (h1.getRateForRegular() - h2.getRateForRegular());
-		}).orElse(null);
-
-		return hotel;
-
-=======
 	public Hotel findCheapBestHotel(LocalDate startDate, LocalDate endDate) {
-		
+
 		int weekdayCount = 0;
 		int weekendCount = 0;
 		for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
@@ -42,15 +27,21 @@ public class HotelReservation {
 		final int weekdays = weekdayCount;
 		final int weekends = weekendCount;
 
-		Hotel hotel = hotelList.stream()
-				.sorted((h1, h2) -> {return h2.getRating() - h1.getRating();})
-				.min((h1, h2) -> {
+		Hotel hotel = hotelList.stream().sorted((h1, h2) -> {
+			return h2.getRating() - h1.getRating();
+		}).min((h1, h2) -> {
 			return ((int) (h1.getRateForWeekday() * weekdays + h1.getRateForWeekend() * weekends)
 					- (int) (h2.getRateForWeekday() * weekdays + h2.getRateForWeekend() * weekends));
 		}).orElse(null);
 
 		return hotel;
->>>>>>> hotel_resevation_system_uc6
+	}
+
+	public Hotel findBestRatedHotel(LocalDate startDate, LocalDate endDate) {
+		Hotel bestHotel = hotelList.stream().sorted((h1, h2) -> {
+			return h2.getRating() - h1.getRating();
+		}).findFirst().orElse(null);
+		return bestHotel;
 	}
 
 	public static void main(String[] args) {
